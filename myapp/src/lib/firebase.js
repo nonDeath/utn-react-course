a9bd10865e204f204ad1aa9e84c6c2b446d5f4c6
Marcelo Gutierrez <marceloaldo@gmail.com>
2023-02-20 19:20:46 -0300

    Added firebase dependencies.

---
+++ myapp/src/lib/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAISo_3RZXnSWsgQNDx-bf4LjS4CiRJj_w",
+  authDomain: "react-utn-a80d4.firebaseapp.com",
+  projectId: "react-utn-a80d4",
+  storageBucket: "react-utn-a80d4.appspot.com",
+  messagingSenderId: "719134604272",
+  appId: "1:719134604272:web:11ff017aa8119470143b35"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
